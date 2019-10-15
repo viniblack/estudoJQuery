74b34afefa35c344a9fc9aa24e33dbf705286986
viniblack <viniibl4ck@gmail.com>
2019-10-15 17:45:54 -0300

    arrumei o cronometro que agr quando chega no zero ele para e arrumei o bug de quando vc clicava entro do campo e fora ele passava o tempo mais rapido

--- public/js/main.js
+++ public/js/main.js
@@ -16,12 +16,13 @@ campo.on("input",function(){
 });
 
 var tempoRestante = $("#tempo-digitacao").text();
-campo.on("focus",function(){
-  setInterval(function(){
+campo.one("focus",function(){
+  var cronometroID = setInterval(function(){
     tempoRestante --;
     $("#tempo-digitacao").text(tempoRestante);
     if(tempoRestante < 1){
       campo.attr("disabled", true);
+      clearInterval(cronometroID);
     }
   },1000);
 });
