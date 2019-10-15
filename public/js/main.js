a7b57959465e9b6664c734967b594f6a5141cf9a
viniblack <viniibl4ck@gmail.com>
2019-10-15 14:43:18 -0300

    add o campo de digitacao e o contador de palavras e de caracteres

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,16 @@ var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
 console.log(tamanhoFrase);
-
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao")
+campo.on("click",function(){
+  var conteudo = (campo.val());
+
+  var qtdPalavras = conteudo.split(" ").length;
+  $("#contador-palavras").text(qtdPalavras);
+
+  var qtdCaracteres = conteudo.length;
+  $("#contador-caracteres").text(qtdCaracteres);
+  
+});
