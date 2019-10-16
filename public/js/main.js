25506aafc26c469b61591b37aa6bdfa130b3e2fe
viniblack <viniibl4ck@gmail.com>
2019-10-16 00:14:34 -0300

    agr o botao reinicializa o cronometro tbm

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao")
 
-$(document).ready(function(){
+$(function(){
   atualizaTamanhoFrase();
   inicializaContadores();
   inicializaCronometro();
@@ -12,7 +12,6 @@ function atualizaTamanhoFrase() {
   var frase = $(".frase").text();
   var numPalavras = frase.split(" ").length;
   var tamanhoFrase = $("#tamanho-frase");
-  console.log(tamanhoFrase);
   tamanhoFrase.text(numPalavras);
 };
 
@@ -48,4 +47,5 @@ function reiniciarJogo(){
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
   };
