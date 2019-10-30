34c54a36e0b3f00796693b7fe7aa60527c547e67
viniblack <viniibl4ck@gmail.com>
2019-10-30 01:11:18 -0300

    add um gif de spiinner para o jogador saber que esta carregando a proxima frase

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,13 +1,17 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
+  $("#spinner").toggle();
   $.get("http://localhost:3000/frases",trocaFraseAleatoria)
   .fail(function(){
     $("#erro").toggle();
     setTimeout(function(){
       $("#erro").toggle();
     }, 2000);
-  });
+  })
+  .always(function(){
+    $("#spinner").toggle();
+  })
 }
 
 function trocaFraseAleatoria(data){
