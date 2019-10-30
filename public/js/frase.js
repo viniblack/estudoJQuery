e00049d5b4fd5b0beb66e0adcd1557682f3f22cf
viniblack <viniibl4ck@gmail.com>
2019-10-30 00:54:55 -0300

    add a mensagem de erro quando o servidor nao funcionar

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,7 +1,13 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-  $.get("http://localhost:3000/frases",trocaFraseAleatoria);
+  $.get("http://localhost:3000/frases",trocaFraseAleatoria)
+  .fail(function(){
+    $("#erro").toggle();
+    setTimeout(function(){
+      $("#erro").toggle();
+    }, 2000);
+  });
 }
 
 function trocaFraseAleatoria(data){
@@ -10,5 +16,4 @@ function trocaFraseAleatoria(data){
   frase.text(data[numeroAleatorio].texto);
   atualizaTamanhoFrase();
   atualizaTempoInicial(data[numeroAleatorio].tempo);
-  
 }
