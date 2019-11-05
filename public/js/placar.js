1e0000317db05776009382b57e87ece0daac8774
viniblack <viniibl4ck@gmail.com>
2019-11-05 12:05:22 -0300

    add um botao que vai gravar no servidor o placar do jogador

--- public/js/placar.js
+++ public/js/placar.js
@@ -1,4 +1,5 @@
 $("#botao-placar").click(mostraPlacar);
+$("#botao-sync").click(sincronizaPlacar);
 
 function inserePlacar(){
   var corpoTabela = $(".placar").find("tbody");
@@ -55,4 +56,9 @@ function removeLinha(){
 
 function mostraPlacar(){
   $(".placar").stop().slideToggle(700);
+}
+
+function sincronizaPlacar(){
+  
+  
 }
